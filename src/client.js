045400a1625fdd2f81538618e7ed72bcb8952b72
mohamed <mouh4945@gmail.com>
2022-07-11 04:40:59 +0100

    client modify

--- src/client.js
+++ src/client.js
@@ -3,6 +3,7 @@ import sanityClient from "@sanity/client";
 export default sanityClient({
   projectId: "6vmvnsm1", // find this at manage.sanity.io or in your sanity.json
   dataset: "production", // this is from those question during 'sanity init'
-  token: 'sanity-auth-token', 
-  useCdn: true,
+  apiVersion: '2021-08-31', // use a UTC date string
+  token: 'sanity-auth-token', // or leave blank for unauthenticated usage
+  useCdn: true, // `false` if you want to ensure fresh data
 });
